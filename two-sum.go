bc2b579a9ada51851f1f7f4d9686eeaba3dcef50
Alvin Tian <tianxuefeng@shuidihuzhu.com>
2019-03-19 14:22:47 +0800

    two-sum

---
+++ two-sum.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func twoSum(nums []int, target int) []int {
+	var m = make(map[int]int)
+	m[nums[0]] = 0
+	for i := 1; i < len(nums); i++ {
+		var c = target - nums[i]
+		if n, ok := m[c]; ok {
+			return []int{n, i}
+		} else {
+			m[nums[i]] = i
+		}
+	}
+	return nil
+}
+
+func main() {
+	fmt.Print(twoSum([]int{2, 7, 11, 15}, 9))
+}
