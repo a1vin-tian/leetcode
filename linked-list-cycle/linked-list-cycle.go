6737f30fafb4e4c102cf06bd504be94f46063da2
Alvin Tian <tianxuefeng@shuidihuzhu.com>
2019-03-29 22:47:24 +0800

    add find link

--- linked-list-cycle/linked-list-cycle.go
+++ linked-list-cycle/linked-list-cycle.go
@@ -9,5 +9,17 @@ type ListNode struct {
  * https://leetcode.com/problems/linked-list-cycle/
  */
 func hasCycle(head *ListNode) bool {
-
+	if head == nil || head.Next == nil {
+		return false
+	}
+	var f = head.Next
+	var s = head
+	for s != f {
+		if f == nil || f.Next == nil {
+			return false
+		}
+		s = s.Next
+		f = f.Next.Next
+	}
+	return true
 }
